f94a3d0be6ea71719e30892bb7b1d32754ef84a9
Nigth <omnes.theo@gmail.com>
2022-04-10 22:34:09 +0200

    use ProductResponse in adapter

--- src/controllers/products/product.response.adapter.ts
+++ src/controllers/products/product.response.adapter.ts
@@ -1,7 +1,8 @@
 import {Product} from "../../services/products/domain/product";
+import {ProductResponse} from "./product.response";
 
 export class ProductResponseAdapter {
-    static adapt(product: Product) {
+    static adapt(product: Product): ProductResponse {
         return {
           id: product.id.value,
           name: product.name,
