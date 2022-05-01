6c241f0282a1b42622a9c373f15b1fa53ecd6cbd
nathan <maerna1@gmail.com>
2022-05-01 15:37:27 +0200

    add order model

---
+++ src/models/orders/order.model.ts
@@ -0,0 +1,50 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+const OrderSchema = new Schema({
+    restaurant: {
+        type: Schema.Types.ObjectId,
+        ref: "restaurants",
+        required: true,
+    },
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: "products",
+        required: true,
+    }],
+    menus: [{
+        type: Schema.Types.ObjectId,
+        ref: "menu",
+        required: false,
+    }],
+    amount: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    status: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    reduction: {
+        type: Schema.Types.ObjectId,
+        ref: "reductions",
+        required: false,
+    }
+}, {
+    collection: "orders",
+    timestamps: true,
+    versionKey: false
+})
+
+export interface OrderProps {
+    _id: string
+    restaurant: string;
+    products: string[];
+    menus?: string[];
+    amount: number;
+    status: number;
+    reductionId?: string;
+}
+
+export type OrderDocument = OrderProps & Document
+
+export const OrderModel = mongoose.model("Order", OrderSchema)
