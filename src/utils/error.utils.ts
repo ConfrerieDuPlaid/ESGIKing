a8a36381bcc83880e31cad6b2c9239f41ab9e95b
Sarah <sschlegel@myges.fr>
2022-05-09 20:33:40 +0200

    remove tests

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -1,3 +1,13 @@
 export class ErrorResponse {
+
+    private static errMsgMap = {
+        400: "Bad request",
+        412: "Precondition failed"
+    }
+
     constructor(public message: string, public status: number) { }
+
+    // public static throwDefaultErr (errNo: number) {
+    //     throw new ErrorResponse(this.errMsgMap[errNo], errNo)
+    // }
 }
