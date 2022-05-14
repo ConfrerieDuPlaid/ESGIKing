df60176f6e2f9cea826e1dece32fd6e0ba85755f
Sarah <sschlegel@myges.fr>
2022-05-14 18:12:08 +0200

    remove unused method

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -1,13 +1,4 @@
 export class ErrorResponse {
 
-    private static errMsgMap = {
-        400: "Bad request",
-        412: "Precondition failed"
-    }
-
     constructor(public message: string, public status: number) { }
-
-    // public static throwDefaultErr (errNo: number) {
-    //     throw new ErrorResponse(this.errMsgMap[errNo], errNo)
-    // }
 }
