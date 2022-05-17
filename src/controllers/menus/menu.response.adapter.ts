f1238ea62a6570164a59a7353c808aab27f9b99f
Sarah <sschlegel@myges.fr>
2022-05-17 21:05:28 +0200

    remove unused adapter if

--- src/controllers/menus/menu.response.adapter.ts
+++ src/controllers/menus/menu.response.adapter.ts
@@ -3,11 +3,9 @@ export class MenuResponseAdapter {
     static adapt(menu: any) {
         let linkToProduct: String[] = [];
         const restaurant = menu.restaurant ? "http://localhost:3001/restaurant/" + menu.restaurant : "";
-        if(menu.products.length > 0){
-            menu.products.forEach((product: any) => {
-                linkToProduct.push("http://localhost:3001/product/" + product.toString())
-            })
-        }
+        menu.products.forEach((product: any) => {
+            linkToProduct.push("http://localhost:3001/product/" + product.toString())
+        })
 
         return {
             name: menu.name,
