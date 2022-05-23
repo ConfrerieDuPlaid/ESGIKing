ab96e8f0f2664a882cc9985533bf19f57bd389fe
Sarah <sschlegel@myges.fr>
2022-05-23 22:54:10 +0200

    comment restaurant controller

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -16,6 +16,55 @@ export class RestaurantController extends DefaultController {
         return router
     }
 
+    /**
+     * Method : PUT
+     * URL : /restaurant
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object with JSON object
+     * {
+     *     "name": "ESGIKing 3",
+     *     "address": "245 rue du Faubourg Saint-Antoine, 75012 Paris",
+     *     "location": {
+     *         "type": "FeatureCollection",
+     *         "version": "draft",
+     *         "features": [
+     *             {
+     *                 "type": "Feature",
+     *                 "geometry": {
+     *                     "type": "Point",
+     *                     "coordinates": [
+     *                         2.382282,
+     *                         48.850217
+     *                     ]
+     *                 },
+     *                 "properties": {
+     *                     "label": "Rue du Faubourg Saint-Antoine 75012 Paris",
+     *                     "score": 0.8153916528925618,
+     *                     "id": "75112_3514",
+     *                     "name": "Rue du Faubourg Saint-Antoine",
+     *                     "postcode": "75012",
+     *                     "citycode": "75112",
+     *                     "x": 654670.63,
+     *                     "y": 6861307.84,
+     *                     "city": "Paris",
+     *                     "district": "Paris 12e Arrondissement",
+     *                     "context": "75, Paris, Île-de-France",
+     *                     "type": "street",
+     *                     "importance": 0.78749
+     *                 }
+     *             }
+     *         ],
+     *         "attribution": "BAN",
+     *         "licence": "ETALAB-2.0",
+     *         "query": "243 rue du Faubourg Saint-Antoine, 75012 Paris",
+     *         "limit": 5
+     *     }
+     * }
+     * @param res HTTP response object
+     *
+     * @returns ResponseDocument or 500 error
+     */
     async createRestaurant (req: Request, res: Response) {
         await super.sendResponse(req, res, async () => {
             await AuthService.getInstance().verifyPermissions(req, Roles.BigBoss)
@@ -26,6 +75,16 @@ export class RestaurantController extends DefaultController {
         }, 201)
     }
 
+    /**
+     * Method : GET
+     * URL : /restaurant/:restaurantId
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object
+     * @param res HTTP response object
+     *
+     * @returns ResponseDocument or 500 error
+     */
     async getOneRestaurant (req: Request, res: Response) {
         await super.sendResponse(req, res, async () => {
             await AuthService.getInstance().verifyPermissions(req, Roles.BigBoss)
@@ -33,6 +92,16 @@ export class RestaurantController extends DefaultController {
         })
     }
 
+    /**
+     * Method : GET
+     * URL : /restaurant
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object
+     * @param res HTTP response object
+     *
+     * @returns ResponseDocument array or 500 error
+     */
     async getAllRestaurants (req: Request, res: Response) {
         await super.sendResponse(req, res, async () => {
             await AuthService.getInstance().verifyPermissions(req, Roles.BigBoss)
@@ -40,6 +109,16 @@ export class RestaurantController extends DefaultController {
         })
     }
 
+    /**
+     * Method : DELETE
+     * URL : /restaurant/:restaurantId
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object
+     * @param res HTTP response object
+     *
+     * @returns Void or 500 error
+     */
     async deleteRestaurant (req: Request, res: Response) {
         await super.sendResponse(req, res, async () => {
             await AuthService.getInstance().verifyPermissions(req, Roles.BigBoss)
@@ -51,6 +130,20 @@ export class RestaurantController extends DefaultController {
         }, 204)
     }
 
+    /**
+     * Method : PATCH
+     * URL : /restaurant/:restaurantId
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object with JSON object
+     * {
+     *     "name": "{{name}}",
+     *     "address": "{{address}}"
+     * }
+     * @param res HTTP response object
+     *
+     * @returns ResponseDocument or 500 error
+     */
     async updateRestaurant (req: Request, res: Response) {
         await super.sendResponse(req, res, async () => {
             await AuthService.getInstance().verifyPermissions(req, Roles.BigBoss)
@@ -62,6 +155,20 @@ export class RestaurantController extends DefaultController {
         }, 204)
     }
 
+    /**
+     * Method : PATCH
+     * URL : /restaurant/addProduct
+     * Permissions required : BigBoss
+     *
+     * @param req HTTP request object with JSON object
+     * {
+     *     "restaurantID" : "{{restaurantId}}",
+     *     "productId" : "{{productId}}"
+     * }
+     * @param res HTTP response object
+     *
+     * @returns RestaurantDocument or 500 error
+     */
     async addAproductInRestaurant(req: Request, res: Response){
         await super.sendResponse(req, res, async () => {
 
